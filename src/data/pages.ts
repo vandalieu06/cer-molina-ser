252908e7b9e86edf06704a7bba197a66ca8378d6
Jhonny Claure <claurejhonny188@gmail.com>
2025-12-28 03:30:17 +0100

    update url

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -6,13 +6,13 @@ interface Page {
 export const pageTitle = "CER Molina Segura";
 
 export const pages: Page[] = [
-  { title: "Inicio", url: "/" },
-  { title: "Sobre Nosotros", url: "/sobre-nosotros" },
-  { title: "Adopciones", url: "/adopciones" },
-  { title: "Colinas Felinas", url: "/colonias-felinas" },
-  { title: "Voluntariado", url: "/voluntariado" },
-  { title: "Hazte Socio/a", url: "/socios" },
-  { title: "Donaciones", url: "/donaciones" },
-  { title: "Recursos", url: "/recursos" },
-  { title: "Contacto", url: "/contacto" },
+  { title: "Inicio", url: "/cer-molina-ser/" },
+  { title: "Sobre Nosotros", url: "/cer-molina-ser/sobre-nosotros" },
+  { title: "Adopciones", url: "/cer-molina-ser/adopciones" },
+  { title: "Colinas Felinas", url: "/cer-molina-ser/colonias-felinas" },
+  { title: "Voluntariado", url: "/cer-molina-ser/voluntariado" },
+  { title: "Hazte Socio/a", url: "/cer-molina-ser/socios" },
+  { title: "Donaciones", url: "/cer-molina-ser/donaciones" },
+  { title: "Recursos", url: "/cer-molina-ser/recursos" },
+  { title: "Contacto", url: "/cer-molina-ser/contacto" },
 ];
